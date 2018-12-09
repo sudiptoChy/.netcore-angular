924cc275cb6fa5df149171054621386676fa1d1b
sudiptoChy <dip.acm77@gmail.com>
2018-12-09 22:43:45 +0600

    added like functionalities

--- myApp.API/Controllers/UsersController.cs
+++ myApp.API/Controllers/UsersController.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 using myApp.API.Data;
 using myApp.API.Dtos;
 using myApp.API.Helpers;
+using myApp.API.Models;
 
 namespace myApp.API.Controllers
 {
@@ -72,6 +73,33 @@ namespace myApp.API.Controllers
                 return NoContent();
 
             throw new Exception($"Updating user {id} failed on save");
+        }
+
+        [HttpPost("{id}/like/{recipientId}")]
+        public async Task<IActionResult> LikeUser(int id, int recipientId)
+        {
+            if (id != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
+
+            var like = await _repo.GetLike(id, recipientId);
+
+            if (like != null)
+                return BadRequest("You already like this user");
+
+            if (await _repo.GetUser(recipientId) == null)
+                return NotFound();
+
+            like = new Like
+            {
+                LikerId = id,
+                LikeeId = recipientId
+            };
+
+            _repo.Add<Like>(like);  
+
+            if (await _repo.SaveAll())
+                return Ok();
+            return BadRequest("Failed to like user.");
         }
     }
 }
